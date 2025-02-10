5cdb815e4e6cf3f9b5f7bf4e50db78553b9479a4
hyungeerhee <hyungeerhee@naver.com>
2025-02-10 11:20:36 +0900

    react study

--- react/shop/src/App.jsx
+++ react/shop/src/App.jsx
@@ -4,37 +4,65 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import bg from "./img/bg.png";
-import data from "./data.jsx";
+import data from "./utils/data.jsx";
 import { useState } from "react";
-import { Product } from "./product.jsx";
+import { Product } from "./components/product.jsx";
+import { Routes, Route, Link, useNavigate, Outlet } from "react-router-dom";
+import { Detail } from "./routes/Detail.jsx";
 // import {Product} from './product.jsx';
 function App() {
   let [shoes, setShoes] = useState(data);
+  let navigate = useNavigate();
   return (
     <div>
       <Navbar bg="dark" data-bs-theme="dark">
         <Container>
           <Navbar.Brand href="#home">REACT SHOP</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <Nav.Link onClick={()=>{navigate('/')}}>Home</Nav.Link>
+            <Nav.Link onClick={()=>{navigate('/detail')}}>Detail</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
-      <div
-        className="main-bg"
-        style={{ backgroundImage: "url(" + bg + ")" }}
-      ></div>
-      <div className="container">
-        <div className="row">
-          {shoes.map(function (a) {
-            return <Product a={a}></Product>;
-          })}
-        </div>
-      </div>
+
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <div>
+              <div
+                className="main-bg"
+                style={{ backgroundImage: "url(" + bg + ")" }}
+              ></div>
+              <div className="container">
+                <div className="row">
+                  {shoes.map(function (a) {
+                    return <Product a={a}></Product>;
+                  })}
+                </div>
+              </div>
+            </div>
+          }
+        />
+        <Route path="/detail" element={<Detail />} />
+        <Route path="/about" element={<div>어바웃페이지임</div>} />
+        <Route path="/event" element={<Event />}>
+          <Route path="one" element={<div>첫 주문시 양배추즙 서비스</div>}></Route>
+          <Route path="two" element={<div>생일기념 쿠폰받기</div>}></Route>
+        </Route>
+
+        <Route path="*" element={<div>404</div>}></Route>
+      </Routes>
     </div>
   );
 }
 
+function Event() {
+  return (
+    <div className="event">
+      <h1>오늘의 이벤트</h1>
+      <Outlet></Outlet>
+    </div>
+  );
+}
 export default App;
