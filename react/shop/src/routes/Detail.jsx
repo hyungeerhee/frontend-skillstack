c11cee4767980f514738b2f2cedfdbc8e8793e9e
hyungeerhee <hyungeerhee@naver.com>
2025-02-11 10:58:58 +0900

    react shop Lifecycle과 useEffect 2

--- react/shop/src/routes/Detail.jsx
+++ react/shop/src/routes/Detail.jsx
@@ -1,12 +1,24 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import styled from "styled-components";
 import { useParams } from "react-router-dom";
+
 function Detail(props) {
+  // let [count, setCount] = useState(0);
+
   let { id } = useParams();
   let find = props.shoes.find(function (x) {
     return x.id == id;
   });
+  let [alert, setAlert] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {setAlert(false)}, 2000);
+  });
   return (
     <div className="container">
+      {alert == true ? (
+        <div className="alert alert-warning">2초이내 구매시 할인</div>
+      ) : null}
       <div className="row">
         <div className="col-md-6">
           <img src={find.url} width="100%" />
@@ -18,8 +30,8 @@ function Detail(props) {
           <button className="btn btn-danger">주문하기</button>
         </div>
       </div>
+      {/* <button onClick={() => {setCount(count + 1)}}>버튼</button> */}
     </div>
   );
 }
-
 export { Detail };
