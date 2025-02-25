4a64ebe41eb21da43169b1d2615a97f2a37b7c5b
hyungeerhee <hyungeerhee@naver.com>
2025-02-25 11:33:27 +0900

    typescript : as문법

--- typescript/index.js
+++ typescript/index.js
@@ -1,7 +1,14 @@
+//1
 function helloName(x) {
   console.log('안녕하세요' + x)
 }
 helloName('이현기')
+//2
 function count(x) {
   return x.toString().length
+}
+//3
+function 내함수(x) {
+  var array = []
+  array[0] = x
 }
